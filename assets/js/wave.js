2347d4bce45869977f280adf580b68d1d15e5309
Jake Tremblay <tremblay.jake@outlook.com>
2022-12-19 13:59:55 -0800

    removed extra buttons

--- assets/js/wave.js
+++ assets/js/wave.js
@@ -10,9 +10,10 @@ let amplitude = new Array(maxwaves);
 let dx = new Array(maxwaves);
 // Value for incrementing X, to be calculated as a function of period and xspacing
 let yvalues;
+let yvalues1;
 // Using an array to store height values for the wave (not entirely necessary)
 function setup() {
-	var c = createCanvas(windowWidth, windowHeight);
+	var c = createCanvas(windowWidth-20, windowHeight);
 	c.parent("header");
 	frameRate(30);
 	colorMode(RGB, 255, 255, 255, 100);
@@ -24,11 +25,13 @@ function setup() {
 		dx[i] = TWO_PI / period * 8;
 	}
 	yvalues = new Array(Math.floor(w / 8));
+	yvalues1 = new Array(Math.floor(w / 8));
 }
 function draw() {
 	background(0);
 	calcWave();
 	renderWave();
+
 }
 function calcWave() {
 	// Increment theta (try different values for 'angular velocity' here
@@ -36,16 +39,20 @@ function calcWave() {
 	// Set all height values to zero
 	for (let i = 0; i < yvalues.length; i++) {
 		yvalues[i] = 0;
+		yvalues1[i] = 0;
 	}
 	// Accumulate wave height values
 	for (let j = 0; j < maxwaves; j++) {
 		let x = theta;
 		for (let i = 0; i < yvalues.length; i++) {
 			// Every other wave is cosine instead of sine
-			if (j % 2 == 0)
-				yvalues[i] += Math.sin(x) * amplitude[j];
-			else
+			if (j % 2 == 0){
 				yvalues[i] += Math.cos(x) * amplitude[j];
+				yvalues1[i] += Math.sin(x) * amplitude[j];
+			} else {
+				yvalues[i] += Math.sin(x) * amplitude[j];
+				yvalues1[i] += Math.cos(x) * amplitude[j];
+			}
 			x += dx[j];
 		}
 	}
@@ -63,4 +70,14 @@ function renderWave() {
 		fill(c);
 		ellipse(x * 8, height / 2 + yvalues[x], 16, 16);
 	}
+
+	for (let x = 0; x < yvalues1.length; x++) {
+    		// Calculate the hue value based on the cell's position
+    	let hue = map(x, 0, yvalues1.length, 0, 255);
+
+    		// Set the fill color using the hue value
+    	let c = color(200, 165, hue);
+    	fill(c);
+    	ellipse(x * 8, height / 2 + yvalues1[x], 16, 16);
+    	}
 }
